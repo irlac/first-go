1aa7971a65db4f03295048ceb44cafad48ff04e0
Camden Richter <richtercamden@gmail.com>
2020-01-27 00:13:02 -0500

    added a remote module

--- first.go
+++ first.go
@@ -3,8 +3,10 @@ package main
 import {
 	"fmt"
 	"github.com/irlac/first-go/morestrings"
+	"github.com/google/go-cmp/cmp"
 }
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello world."))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
