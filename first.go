986121372c0cb07f712ac1460c8bc95f7baf0e8b
Camden Richter <richtercamden@gmail.com>
2020-01-27 00:01:57 -0500

    Added new funcs

--- first.go
+++ first.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import {
+	"fmt"
+	"github.com/irlac/first-go/morestrings"
+}
 
 func main() {
-	fmt.Println("Hello world.")
+	fmt.Println(morestrings.ReverseRunes("Hello world."))
 }
